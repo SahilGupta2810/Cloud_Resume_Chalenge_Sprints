8efb0b406dd547b17486479bffdb02ee49e44699
Sahil Gupta <guptasahil2810@gmail.com>
2023-01-17 00:30:16 +0530

    "Resolved ACM Error"

--- Week-1/Cloud-resume-challenge/lib/cloud-resume-challenge-stack.ts
+++ Week-1/Cloud-resume-challenge/lib/cloud-resume-challenge-stack.ts
@@ -4,9 +4,12 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as S3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam'; 
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import { CfnOutput } from 'aws-cdk-lib';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
 
 
 
@@ -31,16 +34,17 @@ export class CloudResumeChallengeStack extends cdk.Stack {
       destinationBucket: bucket
     });
 
+    //Request a certificate from AWS Certificate Manager and validate it using DNS validation method
+    //or
+    ///Import a certificate from AWS Certificate Manager
+    const certificatearn = "arn:aws:acm:us-east-1:160902316896:certificate/2211b5a3-da32-49f2-a469-0ef394c2298c";
+    const certificate = acm.Certificate.fromCertificateArn(this, 'Import-Certificate', certificatearn)
+
     //Create a Hosted Zone for the domain
     const myHostedZone = new route53.PublicHostedZone(this, 'Hosted Zone for the domain sgupta.cloud', {
       zoneName: 'sgupta.cloud',
       comment: 'Hosted Zone for the domain sgupta.cloud'
     });
-
-    //Request a certificate from AWS Certificate Manager and validate it using DNS validation method
-    //or
-    ///Import a certificate from AWS Certificate Manager
-    const certificate = acm.Certificate.fromCertificateArn(this, 'Import-Certificate', 'arn:aws:acm:us-east-1:160902316896:certificate/d503660c-b2f6-477f-8892-2b36d84d2675')
 
     //Create an OAI 
     const OAI = new cloudfront.OriginAccessIdentity(this, 'OAI', {
@@ -59,7 +63,7 @@ export class CloudResumeChallengeStack extends cdk.Stack {
             {
               isDefaultBehavior: true,
               compress: true,
-              allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+              allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD,
               viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
             }
           ]
@@ -68,14 +72,12 @@ export class CloudResumeChallengeStack extends cdk.Stack {
       viewerCertificate: {
         aliases: ['sgupta.cloud'],
         props: {
-          acmCertificateArn: certificate.certificateArn,  // optional
-          sslSupportMethod: 'sni-only',
-          minimumProtocolVersion: 'TLSv1.2_2019', 
+          acmCertificateArn: certificatearn,  // optional
+          sslSupportMethod: 'sni-only', // optional',
+          minimumProtocolVersion: 'TLSv1.2_2021', 
           // All `props` options here: https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.CfnDistribution.ViewerCertificateProperty.html
         }
       },
-
-      //viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate),
       defaultRootObject: 'index.html',
       comment: "Distributon for Cloud Resume Challenge",
       errorConfigurations: [
@@ -88,37 +90,28 @@ export class CloudResumeChallengeStack extends cdk.Stack {
 
     });
 
-    //TEST THIS -------- cREATE A NORMAL DISTRIBUTION
-    // const distribution = new cloudfront.Distribution(this, 'CloudFrontDistribution', {
-    //     defaultBehavior: {
-          
-    //     }
-    // })
-
-
     //TEST THIS -------- Create Route53 Records
-    new route53.ARecord(this, 'Alias', {
-      zone: myHostedZone,
-      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
-    });
+    // new route53.ARecord(this, 'Alias', {
+    //   zone: myHostedZone,
+    //   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    // });
 
-      
-
+    new route53.ARecord(this, 'A-record', {
+        zone: myHostedZone,
+        recordName: 'sgupta.cloud',
+        target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    })
 
     new CfnOutput(this, 'BucketName', {
       value: bucket.bucketName,
       description: 'Bucket Name'
     });
 
-    // new CfnOutput(this, 'OAI', {
-    //   value: OAI.originAccessIdentityId,
-    //   description: 'OAI ID'
-    // });
 
-    new CfnOutput(this, 'DistributionId', {
-      value: distribution.distributionDomainName,
-      description: 'Distribution ID'
-    });
+    // new CfnOutput(this, 'DistributionId', {
+    //   value: distribution.distributionDomainName,
+    //   description: 'Distribution ID'
+    // });
 
 
   }
